24b840ba1120b3fde2df25deaab1bb436c3ba650
nizbridge <nizbridge@gmail.com>
2022-05-10 12:41:50 +0900

    Update index.js
    fix

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import Run from './Run';
 
 
-ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            <Route exact path="/" component={App}/>
-            <Route path="/Run" component={Run}/>
-        </div>
-    </BrowserRouter>,
-    document.getElementById('root');
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <div>
+//             <Route exact path="/" component={App}/>
+//             <Route path="/Run" component={Run}/>
+//         </div>
+//     </BrowserRouter>,
+//     document.getElementById('root');
+// );
 
