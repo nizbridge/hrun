45fdb7cc7ed376ba652ddfc9144dadf69ee92afd
nizbridge <nizbridge@gmail.com>
2022-05-10 13:16:12 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,8 @@ module.exports = {
     publicPath: '/static/'
   },
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['', '.js', '.jsx', '.css']
+    
   },
   devtool: 'eval-source-map',
   plugins: [
