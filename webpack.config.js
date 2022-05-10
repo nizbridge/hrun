e31a18b0cb4aa784ba54293a76c7d38ef16b5832
nizbridge <nizbridge@gmail.com>
2022-05-10 13:04:11 +0900

    Update webpack.config.js
    fix

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel',
                 query: {
                     presets: ['react', 'es2015','stage-1']
                 }
